a3cfa65190d626413307f3ae933b5ea64b5e7e94
Aaron Gibson <aarongibson86@gmail.com>
2019-07-16 11:52:49 -0500

    messing around with bootstrap

---
+++ client/src/components/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+	render() {
+		return (
+			<div className="container bg-info">
+				<button className="btn btn-success btn-lg">TEST</button>
+			</div>
+		);
+	}
+}
